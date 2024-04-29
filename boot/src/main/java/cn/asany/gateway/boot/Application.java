1f0b78e65642aefb62ce06ef8aea8ab01e15fd8b
李茂峰 <limaofeng@msn.com>
2024-04-29 14:17:17 +0800

    feat: update GraphQL gateway config file path

--- boot/src/main/java/cn/asany/gateway/boot/Application.java
+++ boot/src/main/java/cn/asany/gateway/boot/Application.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2024 Asany
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.asany.net/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.asany.gateway.boot;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -61,7 +76,7 @@ public class Application extends SpringBootServletInitializer {
         .schema(schemaParser.makeExecutableSchema())
         .clientFactory(templateFactory)
         .scalarResolver(new ScalarTypeResolver(scalarFactory))
-        .config("classpath:graphql-gateway.yaml")
+        .config("/app/config/graphql-gateway.yaml")
         .build();
   }
 
